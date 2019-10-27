37f62e0c038faf5e3a9f03f550392c61c3770a4d
Oukey <oukey311@gmail.com>
2019-10-27 17:54:33 +0400

    Первый скрапер

--- First_scraper/scrapetest.py
+++ First_scraper/scrapetest.py
@@ -1,4 +1,24 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+
+def getTitle(url):
+    '''Функция возвращает название страницы или None'''
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs0bj = BeautifulSoup(html.read())
+        title = bs0bj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+if title is None:
+    print('Название не найдено')
+else:
+    print(title)
